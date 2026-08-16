b0e60f4d960e86c41ddd0d2e94e3a823e9bac278
thecousinsmobile-art <thecousinsmobile@gmail.com>
2026-08-16 22:19:52 +0100

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,9 @@
+-- Whitelist: add usernames and their personal keys here
+-- Format: ["username"] = "key"
+return {
+    ["YourUsername"] = "yourkey",
+    ["Friend1"] = "friend1key",
+    ["Friend2"] = "friend2key",
+    -- Add more users below
+    -- ["AnotherUser"] = "anotherkey",
+}
