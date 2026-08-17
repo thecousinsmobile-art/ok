fe0437206043513b48076d2037b9fde929d3fdab
thecousinsmobile-art <thecousinsmobile@gmail.com>
2026-08-17 13:08:20 +0100

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,14 +1,87 @@
+-- whitelist.lua
 return {
-    -- Direct whitelist (no key needed)
-    usernames = {
-        ["ClaysRetake"] = "lifetime",
-    },
-
-    -- Keys: key -> { username, expiry }
-    keys = {
-        ["123"] = {
-            username = "ClaysRetake",
-            expiry = "lifetime"
+    -- Key Database with 5 users for each duration type (1h, 1d, lifetime)
+    Keys = {
+        -- ==========================
+        -- 1 HOUR KEYS (5 Users)
+        -- ==========================
+        ["KEY_1H_USER1"] = {
+            owner = "user1",
+            type = "1h",
+            expires = 1787053600 -- Replace with a future Unix timestamp (Current Time + 1 hour)
         },
+        ["KEY_1H_USER2"] = {
+            owner = "user2",
+            type = "1h",
+            expires = 1787053600
+        },
+        ["KEY_1H_USER3"] = {
+            owner = "user3",
+            type = "1h",
+            expires = 1787053600
+        },
+        ["KEY_1H_USER4"] = {
+            owner = "user4",
+            type = "1h",
+            expires = 1787053600
+        },
+        ["KEY_1H_USER5"] = {
+            owner = "user5",
+            type = "1h",
+            expires = 1787053600
+        },
+
+        -- ==========================
+        -- 1 DAY KEYS (5 Users)
+        -- ==========================
+        ["KEY_1D_USER1"] = {
+            owner = "user1",
+            type = "1d",
+            expires = 1787136000 -- Replace with a future Unix timestamp (Current Time + 24 hours)
+        },
+        ["KEY_1D_USER2"] = {
+            owner = "user2",
+            type = "1d",
+            expires = 1787136000
+        },
+        ["KEY_1D_USER3"] = {
+            owner = "user3",
+            type = "1d",
+            expires = 1787136000
+        },
+        ["KEY_1D_USER4"] = {
+            owner = "user4",
+            type = "1d",
+            expires = 1787136000
+        },
+        ["KEY_1D_USER5"] = {
+            owner = "user5",
+            type = "1d",
+            expires = 1787136000
+        },
+
+        -- ==========================
+        -- LIFETIME KEYS (5 Users)
+        -- ==========================
+        ["LIFETIME_KEY_USER1"] = {
+            owner = "ClaysRetake",
+            type = "lifetime"
+        },
+        ["LIFETIME_KEY_USER2"] = {
+            owner = "user2",
+            type = "lifetime"
+        },
+        ["LIFETIME_KEY_USER3"] = {
+            owner = "user3",
+            type = "lifetime"
+        },
+        ["LIFETIME_KEY_USER4"] = {
+            owner = "user4",
+            type = "lifetime"
+        },
+        ["LIFETIME_KEY_USER5"] = {
+            owner = "user5",
+            type = "lifetime"
+        }
     }
 }
